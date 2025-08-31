aaec338129ea0ca2c16da2dd07c565cc243f952c
Patryk Kurdziel <patryk.kurdziel@protonmail.com>
2025-08-31 07:20:45 +0200

    build: Add checksums for macOS binaries

--- Formula/music-discord-rpc.rb
+++ Formula/music-discord-rpc.rb
@@ -8,12 +8,12 @@ class MusicDiscordRpc < Formula
 
   on_intel do
     url "https://github.com/patryk-ku/music-discord-rpc/releases/download/v#{version}/music-discord-rpc-macos-amd64.tar.gz"
-    sha256 ""
+    sha256 "a3f6a36627ac4a4c40ce563daf3e908b4f53efac9ec909b12f7c77ae213d7dc1"
   end
 
   on_arm do
     url "https://github.com/patryk-ku/music-discord-rpc/releases/download/v#{version}/music-discord-rpc-macos-arm64.tar.gz"
-    sha256 ""
+    sha256 "214eecce687f1545ef56c95d2bd2a8d6dfd2e083ae9f8ea5089ea31ef552fa9b"
   end
 
   def install
